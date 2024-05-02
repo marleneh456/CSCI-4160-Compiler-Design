22d90030b9d445a11c4a3a1b9a249ac909a0d578
Marlene Habib <82187174+marleneh456@users.noreply.github.com>
2024-05-01 19:22:47 -0500

    Add files via upload

---
+++ Projects/Project5/Windows/AST_Evaluation/slp.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include "slp.h"
+
+
+using namespace std;
+
+/* To be implemented by stduents */
+void CompoundStm::interp( SymbolTable& symbols )
+{
+	/*implementation here*/
+}
+
+
+/* To be implemented by stduents */
+void AssignStm::interp( SymbolTable& symbols )
+{
+	/*implementation here*/
+}
+
+
+
+void PrintStm::interp( SymbolTable& symbols )
+{
+	/*implementation here*/
+}
+
+
+int IdExp::interp( SymbolTable& symbols )
+{
+	/*implementation here*/
+}
+
+
+int NumExp::interp( SymbolTable& symbols )
+{
+	/*implementation here*/
+}
+
+
+int OpExp::interp( SymbolTable& symbols )
+{
+	/*implementation here*/
+}
+
+
+
+int EseqExp::interp( SymbolTable& symbols )
+{
+	/*implementation here*/
+}
+
+
+void PairExpList::interp( SymbolTable& symbols)
+{
+	/*implementation here*/
+}
+
+
+void LastExpList::interp( SymbolTable& symbols)
+{
+	/*implementation here*/
+}
+
